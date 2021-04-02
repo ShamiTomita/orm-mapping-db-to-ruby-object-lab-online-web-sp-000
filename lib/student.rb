b049109e1c8d293734318f4c832023b3127d1a64
Shami Tomita <shamitomita@gmail.com>
2021-04-02 22:07:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,8 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL 
+    SELECT 
   end
   
   def save
