5ce5e32b2bafe7bbdc1553e509331de3a4ded44e
Shami Tomita <shamitomita@gmail.com>
2021-04-02 22:20:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,7 @@ class Student
     end 
   end
   
-  def self.first_student_in_grade_10(x)
+  def self.first_student_in_grade_10
     sql = <<-SQL 
     SELECT * 
     FROM students 
@@ -79,7 +79,7 @@ class Student
     LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end 
   end
