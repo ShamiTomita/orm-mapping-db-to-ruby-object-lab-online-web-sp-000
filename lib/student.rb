47baa223fabab8408d382d1a3e3783fca7ac3d58
Shami Tomita <shamitomita@gmail.com>
2021-04-02 22:04:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,10 @@
 class Student
   attr_accessor :id, :name, :grade
-
+  def initialize
+    @id = id 
+    @name = name 
+    @grade = grade 
+  end 
   def self.new_from_db(row)
     new_student = self.new 
     new_student.id = row[0]
