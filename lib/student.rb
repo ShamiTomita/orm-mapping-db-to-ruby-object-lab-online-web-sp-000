4350a8939b8e5d857af3f9802e4dc75e88c6cf78
Shami Tomita <shamitomita@gmail.com>
2021-04-02 22:15:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,10 +63,10 @@ class Student
     SELECT * 
     FROM students 
     WHERE grade = 10 
-    LIMIT X
+    LIMIT = ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, X).map do |row|
       self.new_from_db(row)
     end 
   end
